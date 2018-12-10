c9152e5471dfe7e639db76ceeaa6b221c0a491ef
robertosabino <44299817+robertosabino@users.noreply.github.com>
2018-12-10 10:16:42 -0200

    Add files via upload

---
+++ AspCore_Leilao/Startup.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using AspCore_Roles.Data;
+using AspCore_Roles.Models;
+using AspCore_Roles.Services;
+
+namespace AspCore_Roles
+{
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddDbContext<ApplicationDbContext>(options =>
+          options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+      services.AddDbContext<LeilaoDbContext>(options =>
+          options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+      services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+      // Add application services.
+      services.AddTransient<IEmailSender, EmailSender>();
+
+      services.AddMvc();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider, LeilaoDbContext context)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseBrowserLink();
+        app.UseDeveloperExceptionPage();
+        app.UseDatabaseErrorPage();
+      }
+      else
+      {
+        app.UseExceptionHandler("/Home/Error");
+      }
+
+      app.UseStaticFiles();
+
+      app.UseAuthentication();
+
+      app.UseMvc(routes =>
+      {
+        routes.MapRoute(
+                  name: "default",
+                  template: "{controller=Home}/{action=Index}/{id?}");
+      });
+
+      //CreateRoles(serviceProvider).Wait();
+      //DbInitializer.Initialize(context);
+    }
+
+    private async Task CreateRoles(IServiceProvider serviceProvider)
+    {
+      var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+      var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+      string[] rolesNames = { "Admin", "User", "Operator" };
+      IdentityResult result;
+      foreach (var namesRole in rolesNames)
+      {
+        var roleExist = await roleManager.RoleExistsAsync(namesRole);
+        if (!roleExist)
+        {
+          result = await roleManager.CreateAsync(new IdentityRole(namesRole));
+        }
+      }
+    }
+  }
+}
